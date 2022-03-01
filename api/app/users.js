ef0ef759dd11a5c4291f9a1dfb66d37d7db2222b
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-03-01 10:04:09 +0600

    Realization of getting and posting users

---
+++ api/app/users.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const User = require('../models/User');
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    try {
+        const Users = await User.find();
+        res.send(Users);
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.post('/',async (req, res, next) => {
+    try {
+        if (!req.body.username) {
+            return res.status(400).send({error: 'Fill in username'});
+        }
+        const userData = {
+            username: req.body.username,
+        }
+
+        const user = new User(userData);
+        await user.save();
+        return res.send({user: user});
+    } catch (e) {
+        console.log('error');
+        next(e);
+    }
+});
+
+module.exports = router;
---
+++ api/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+const config = require('./config');
+const users = require('./app/users');
+// const tasks = require('./app/tasks');
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/users', (users));
+// app.use('/tasks', (tasks));
+
+const run = async () => {
+    await mongoose.connect(config.mongo.db, config.mongo.options);
+
+    app.listen(port, () => {
+        console.log(`Server is listening port ${port}...`);
+    });
+
+    process.on('exit', () => {
+        mongoose.disconnect();
+    })
+}
+
+run().catch(e => console.log(e));
+
